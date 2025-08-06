814c686c3390f336e3d0e28d6b72cc2a2be89dd9
Jakkaphong <69631237+jakkapong5643@users.noreply.github.com>
2025-08-06 21:49:17 +0700

    Update configs.py

--- Agentic_AI/configs.py
+++ Agentic_AI/configs.py
@@ -1,6 +1,7 @@
 settings = {
-    "google_gemini_api_key": "AIzaSyBLiRSu2Br2r67uQIR2I7lVg0WYeFUrMfY",
+    "google_gemini_api_key": "***",
     "temperature": 0.3,
     "quality_threshold": 7,
     "max_feedback_rounds": 3
 }
+
